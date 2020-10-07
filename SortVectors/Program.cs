ba3bda8badb69f98884fdb357cf1ffda2aea318d
Filippo Omicini <FILIPPO.OMICINI@local.ittcesena.it>
2020-10-07 11:47:32 +0200

    Second Step Ordinamento

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -7,13 +7,18 @@ namespace SortVectors
         static void Main(string[] args)
         {
             int[] vet = new int [10] { 1,43,65,-43,-54,2,4,45,-34,76};
-            Array.Sort(vet);
+            Ordina(vet);
             for (int i = 0; i < vet.Length; i++)
             {
                 Console.WriteLine(vet[i]);
             }
 
             Console.ReadKey();
+        }
+
+        public static void Ordina(int[] v)
+        {
+            Array.Sort(v);
         }
     }
 }
