b9c21e3deb23b16c2e5bfb1667dd454a7ae4846e
Filippo Omicini <FILIPPO.OMICINI@local.ittcesena.it>
2020-10-07 11:06:10 +0200

    first step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int [10] { 1,43,65,-43,-54,2,4,45,-34,76};
+            Array.Sort(vet);
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
